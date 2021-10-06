d25f5c3ec55e3e9a9561aa5fdc8a61120e4e20df
m-mine <m-mine@10antz.co.jp>
2021-10-06 19:43:25 +0900

    add auto generate orm

--- tests/select_to_json_test.go
+++ tests/select_to_json_test.go
@@ -54,6 +54,63 @@ func TestSelectToJson(t *testing.T) {
 	}
 }
 
+func TestAutoGenerateOrm(t *testing.T) {
+	url := "projects/spanner-emulator/instances/test/databases/test"
+
+	ctx := context.Background()
+
+	client, _ := spanner.NewClient(ctx, url)
+	defer client.Close()
+
+	var columnTable []*ColumnTable
+
+	rtx := client.ReadOnlyTransaction()
+	defer rtx.Close()
+
+	tableName := "Singers"
+	query := fmt.Sprintf("SELECT t.column_name as ColumnName, t.spanner_type as SpannerType, FROM information_schema.columns AS t WHERE t.table_name = \"%s\"  order by t.ordinal_position", tableName)
+	err := ssorm.SimpleQueryRead(ctx, rtx, query, &columnTable)
+
+	dataTypes := make(map[string]string)
+	columnNames := []string{}
+	for i := 0; i < len(columnTable); i++ {
+		columnName := columnTable[i].ColumnName
+		dataType := columnTable[i].SpannerType
+		dataTypes[columnName] = dataType
+		columnNames = append(columnNames, columnName)
+	}
+
+	generate := fmt.Sprintf("type %s struct {\n", tableName)
+	result := extractColumn(dataTypes)
+	for i := 0; i < len(columnNames); i++ {
+		k := columnNames[i]
+		v := result[k]
+		generate += fmt.Sprintf("\t%s %s\n", k, v)
+	}
+	generate += "}"
+	fmt.Println(generate)
+
+	//type Singer struct {
+	//	SingerId        int64  `ssorm_key:"primary"`
+	//	FirstName       string `spanner:"Name"`
+	//	LastName        spanner.NullString
+	//	LastName2       spanner.NullString
+	//	TestTime        spanner.NullTime     `spanner:"TestTime"` //NULL を許容する場合必ず、spanner.NullTimeを指定すること
+	//	TestSpannerTime spanner.NullTime     `spanner:"TestSpannerTime"`
+	//	TagIDs          []spanner.NullString `spanner:"TagIds"`
+	//	Numbers         []int64              `spanner:"Numbers"`
+	//	Albums          []*Albums            `spanner:"Albums"`
+	//	Concerts        []*Concerts          `spanner:"Concerts"`
+	//	DeleteTime      spanner.NullTime     `spanner:"DeleteTime" ssorm_key:"delete_time"`
+	//	CreateTime      time.Time            `spanner:"CreateTime" ssorm_key:"create_time"`
+	//	UpdateTime      time.Time            `spanner:"UpdateTime" ssorm_key:"update_time"`
+	//}
+
+	if err != nil {
+		t.Fatalf("Error happened when search singers, got %v", err)
+	}
+}
+
 func readRows(iter *spanner.RowIterator) []spanner.Row {
 	var rows []spanner.Row
 	defer iter.Stop()
@@ -160,4 +217,61 @@ func extractDataByType(types map[string]string, rows []spanner.Row) []*map[strin
 	}
 	//log.Println("parquet format: %s", md)
 	return result
+}
+
+func extractColumn(types map[string]string) map[string]string {
+
+	valueMap := make(map[string]string)
+	var (
+		dataType   string
+		spannerTag string
+	)
+	for k, v := range types {
+		ormTag := ""
+		spannerTag = fmt.Sprintf("%s:\"%s\"", "spanner", k)
+		if strings.Index(v, "STRING") == 0 {
+			dataType = "spanner.NullString"
+		}
+		if strings.Index(v, "ARRAY") == 0 {
+			if strings.Index(v, "STRING") > 0 {
+				dataType = "[]spanner.NullString"
+			}
+			if strings.Index(v, "INT") > 0 {
+				dataType = "[]spanner.NullInt64"
+			}
+			if strings.Index(v, "FLOAT") > 0 {
+				dataType = "[]spanner.NullFloat64"
+			}
+
+		}
+
+		switch v {
+		case "TIMESTAMP":
+			dataType = "spanner.NullTime"
+		case "INT64":
+			dataType = "spanner.NullInt64"
+		case "FLOAT64":
+			dataType = "spanner.NullFloat64"
+		case "BOOL":
+			dataType = "bool"
+		}
+
+		switch k {
+		case "CreateTime":
+			ormTag = "ssorm_key:\"create_time\""
+		case "UpdateTime":
+			ormTag = "ssorm_key:\"update_time\""
+		case "DeleteTime":
+			ormTag = "ssorm_key:\"delete_time\""
+
+		}
+		if ormTag == "" {
+			valueMap[k] = fmt.Sprintf("%s `%s`", dataType, spannerTag)
+		} else {
+			valueMap[k] = fmt.Sprintf("%s `%s %s`", dataType, spannerTag, ormTag)
+		}
+	}
+
+	//log.Println("parquet format: %s", md)
+	return valueMap
 }
